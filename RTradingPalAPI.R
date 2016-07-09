6863f645254609c04c86bd158a8e7b21474b0572
IFFranciscoME <if.francisco.me@gmail.com>
2016-07-09 15:46:21 -0500

    GetToken agregada al RTradingPal.R

--- RTradingPalAPI.R
+++ RTradingPalAPI.R
@@ -5,13 +5,41 @@
 # -- License: GNU General Public License ------------------------------------------ -- #
 # -- ------------------------------------------------------------------------------ -- #
 
+rm(list=ls()) # Remover objetos del Environment
+cat("\014")   # Limpiar consola
+
+pkg <- c("base","zoo","RCurl","jsonlite")
+
+inst <- pkg %in% installed.packages()
+if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
+instpackages <- lapply(pkg, library, character.only=TRUE)
+
 # -- Lista de funciones contenidas en este codigo para API Trading Pal ------------ -- #
 
 # -- Obtener Token de validacion ------------------------------------------------ ---- #
 # -- --------------------------------------------------- GET /token?token=[token] --1- #
 # -- ---------------------------------------------------------------------------- ---- #
 
-GetToken <- function(){}
+GetToken <- function(Email, Pass){
+
+http  <- "www.tradingpal.com/api/auth?email="
+http1 <- paste(http,Email,sep="")
+http2 <- paste("&password=",Pass,sep="")
+httpF <- paste(http1,http2,sep="")
+
+Query   <- getURL(httpF, cainfo = system.file("CurlSSL","cacert.pem", package="RCurl"))
+RetJson <- fromJSON(Query, simplifyDataFrame = TRUE)
+
+UserId <- RetJson$uid
+Token  <- RetJson$token
+TokenE <- as.POSIXct(RetJson$auth$token$exp, origin ="1970-01-01")
+
+DataM  <- data.frame(RetJson$uid,RetJson$token,
+                    as.POSIXct(RetJson$auth$token$auth_time, origin = "1970-01-01"),
+                    as.POSIXct(RetJson$expires, origin = "1970-01-01"))
+colnames(DataM) <- c("UserId","Token","AuthTime","AuthExp")
+return(DataM)
+}
 
 # -- Obtener Informacion/Precio actual de Instrumento --------------------------- ---- #
 # -- -------------------------------------------------------------- GET /[symbol] --2- #
